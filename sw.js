49bd6f3e4fb474abe636586c9eeeb146a529314c
sustento-e-provisaoipr-ipr <sustentoeprovisaoipr@gmail.com>
2026-09-18 11:31:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
-const CACHE_NAME = 'sustento-provisao-v1';
+const CACHE_NAME = 'sustento-v2';
 const urlsToCache = [
-  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Força a atualização imediata do PWA
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
